7c993597914d37f9385c96f87f2d99530c161c99
thomas.li <4167023@qq.com>
2019-02-15 15:32:19 +0800

    modify to save predict

--- sinobot_ctpn/ctpn_helper.py
+++ sinobot_ctpn/ctpn_helper.py
@@ -9,8 +9,7 @@ from sinobot_ctpn.nets import model_train as model
 import numpy as np
 from sinobot_ctpn.utils.rpn_msr.proposal_layer import proposal_layer
 from sinobot_ctpn.utils.text_connector.detectors import TextDetector
-tf.app.flags.DEFINE_string('gpu', '0', '')
-tf.app.flags.DEFINE_string('checkpoint_path', '/usr/share/ctpn/model/', '')
+tf.app.flags.DEFINE_string('checkpoint_path', '/home/luhya/Documents/src/myctpn/checkpoints_mlt/', '')
 FLAGS = tf.app.flags.FLAGS
 
 def resize_image(img):
@@ -33,16 +32,12 @@ def resize_image(img):
 
 
 def get_ctpn_boxes(table_img):
-    tf.reset_default_graph()
     with tf.get_default_graph().as_default():
         input_image = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_image')
         input_im_info = tf.placeholder(tf.float32, shape=[None, 3], name='input_im_info')
-        # with tf.variable_scope('global_step',reuse = tf.AUTO_REUSE):
-        # try:
+
         global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
-        # except ValueError:
-        #     tf.get_variable_scope().reuse_variables()  
-            
+
         bbox_pred, cls_pred, cls_prob = model.model(input_image)
 
         variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
@@ -54,7 +49,6 @@ def get_ctpn_boxes(table_img):
             model_path = os.path.join(FLAGS.checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
             print('Restore from {}'.format(model_path))
             saver.restore(sess, model_path)
-            table_img = table_img[:, :, ::-1]
             img, (rh, rw) = resize_image(table_img)
             h, w, c = img.shape
             im_info = np.array([h, w, c]).reshape([1, 3])
@@ -69,8 +63,12 @@ def get_ctpn_boxes(table_img):
             textdetector = TextDetector(DETECT_MODE='H')
             boxes = textdetector.detect(textsegs, scores[:, np.newaxis], img.shape[:2])
             boxes = np.array(boxes, dtype=np.int)
+            showed_img = img.copy()
+            for i, box in enumerate(boxes):
+                    coordinate_box = [box[:8].astype(np.int32).reshape((-1, 1, 2))]
+                    cv2.polylines(showed_img, coordinate_box, True, color=(0, 255, 0), thickness=2)
+            showed_img = cv2.resize(showed_img, None, None, fx=1.0 / rh, fy=1.0 / rw, interpolation=cv2.INTER_LINEAR)        
             cost_time = (time.time() - start)
             print("cost time: {:.2f}s".format(cost_time))
-            # tf.get_variable_scope().reuse_variables()
-            return boxes,img
+            return boxes,img,showed_img
 
